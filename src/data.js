ea1d395c15d6615ddac9053f41cdf4cdbe56ba68
jmurrietabvc <127762653+jmurrietabvc@users.noreply.github.com>
2023-10-30 12:05:29 -0500

    Update to the Course Registration
    Added the update from @gaconza to the Course Registration component and added the rendering to the page

--- src/data.js
+++ src/data.js
@@ -1,148 +1,108 @@
 const coursesData = [
-  // Term 1 Courses
-  {
-    id: 1,
-    name: "Programming Fundamentals",
-    code: "SODV1101",
-    description:
-      "Computer programming is central to software development...",
-  },
-  {
-    id: 2,
-    name: "Web and Internet Fundamentals",
-    code: "TECH1101",
-    description:
-      "Learners use HyperText Markup Language (HTML), Cascading Style Sheets...",
-  },
-  {
-    id: 3,
-    name: "Internet of Things",
-    code: "TECH1102",
-    description:
-      "This course focuses on the pervasiveness of the internet...",
-  },
-  {
-    id: 4,
-    name: "Essential Skills for Teams Collaboration",
-    code: "MGMT1103",
-    description:
-      "This course prepares learners with practical skills in effective communications...",
-  },
-  {
-    id: 5,
-    name: "Math for the Computer Industry",
-    code: "MATH1901",
-    description:
-      "This course provides learners with the practical knowledge and skills...",
-  },
-  // Term 2 Courses
-  {
-    id: 6,
-    name: "Introduction to Relational Databases",
-    code: "DATA1201",
-    description:
-      "In this course, the learner plans, creates and manages a relational database...",
-  },
-  {
-    id: 7,
-    name: "User Experience Design",
-    code: "DESN2301",
-    description:
-      "This course introduces the principles of broad user experience design...",
-  },
-  {
-    id: 8,
-    name: "Introduction to Web Programming",
-    code: "SODV1201",
-    description:
-      "This course provides learners with foundational web programming knowledge...",
-  },
-  {
-    id: 9,
-    name: "Introduction to Object Oriented Programming",
-    code: "SODV1202",
-    description:
-      "In this course, learners apply principles of design, logic, and structure...",
-  },
-  {
-    id: 10,
-    name: "Networking Essentials",
-    code: "TECH1201",
-    description:
-      "Network systems interconnect computer-related resources, services, and users...",
-  },
-  // Term 3 Courses
-  {
-    id: 11,
-    name: "Relational Databases",
-    code: "DATA2201",
-    description:
-      "In this course learners use Structured Query Language (SQL) on commercial...",
-  },
-  {
-    id: 12,
-    name: "Project Management in Software Development",
-    code: "MGMT1104",
-    description:
-      "This course introduces the fundamental principles necessary for successful...",
-  },
-  {
-    id: 13,
-    name: "Rapid Application Development",
-    code: "SODV2101",
-    description:
-      "This course introduces programming techniques supported by a visual rapid application...",
-  },
-  {
-    id: 14,
-    name: "Web Programming",
-    code: "SODV2201",
-    description:
-      "This course gives learners the opportunity to enhance and enrich their skills...",
-  },
-  {
-    id: 15,
-    name: "Object Oriented Programming",
-    code: "SODV2202",
-    description:
-      "In this course, learners explore object-oriented design concepts and techniques...",
-  },
-  {
-    id: 16,
-    name: "Introduction to Game and Simulation Programming",
-    code: "SODV2203",
-    description:
-      "Building on programming skills obtained in previous courses...",
-  },
-  {
-    id: 17,
-    name: "Algorithms and Data Structures",
-    code: "SODV2401",
-    description:
-      "This course introduces learners to fundamental abstract data types and common techniques...",
-  },
-  {
-    id: 18,
-    name: "Software Development Diploma Capstone Project",
-    code: "SODV2999",
-    description:
-      "Learners strengthen their skills and their knowledge of software development by creating a portfolio...",
-  },
-  {
-    id: 19,
-    name: "Mobile Application Development",
-    code: "SODV3203",
-    description:
-      "Learners approach application development with a mobile-first and mobile-only lens...",
-  },
-  {
-    id: 20,
-    name: "Enterprise Computing",
-    code: "TECH2102",
-    description:
-      "Learners will learn about the protocols and applications that make possible...",
-  },
-  // Add more courses as needed
+// Term 1 Courses
+{
+  id: 1,
+  name: "Project management1",
+  code: "Pr111",
+  description:
+    "This course provides a comprehensive introduction to the essential principles and practices of project management.Whether you're a novice or looking to brush up on your skills, you'll gain a solid foundation in planning, execution, and monitoring of projects, equipping you with the knowledge and tools needed to successfully lead and contribute to a wide range of endeavors.",
+},
+{
+  id: 2,
+  name: "C++ Programming Fundamentals",
+  code: "C++111",
+  description:
+    "In this course, you will delve into the core concepts of C++, including data types, variables, control structures, functions, and object-oriented programming principles. You'll gain practical experience through hands-on coding exercises, projects, and real-world applications. ",
+},
+{
+  id: 3,
+  name: "Computer Maintenance",
+  code: "CompM1111",
+  description:
+    "Discover the essential skills to keep your computer running smoothly with our Computer Maintenance course. Whether you're a tech enthusiast, a business professional, or just an everyday computer user, this course is designed to empower you with the knowledge and techniques needed to maintain and troubleshoot your computer hardware and software. ",
+},
+{
+  id: 4,
+  name: "Information Security1",
+  code: "IS1111",
+  description:
+    "This course provides a comprehensive introduction to the principles, concepts, and practices of safeguarding sensitive data and systems. Explore the fundamentals of cybersecurity, risk management, encryption, and threat detection. Gain the knowledge and skills to protect valuable information assets and mitigate digital threats. ",
+},
+
+// Term 2 Courses 
+{
+  id: 5,
+  name: "Networking",
+  code: "Net222",
+  description:
+    "Dive into the fundamental concepts and practical skills essential for building and managing robust computer networks. From understanding network topologies to configuring routers and switches, this course equips you with the knowledge and hands-on experience needed to excel in the dynamic field of Information Technology.",
+},
+{
+  id: 6,
+  name: "Web technology",
+  code: "Web222",
+  description:
+    "From the fundamentals of HTML and CSS to the latest advancements in web development, this program equips you with the knowledge and skills needed to create and manage cutting-edge websites. Learn about responsive design, JavaScript, content management systems, and emerging trends in the digital landscape. ",
+},
+{
+  id: 7,
+  name: "Project Management2",
+  code: "Pr222",
+  description:
+    "This course is designed to take your project management skills to the next level. Explore advanced techniques, tools, and strategies to successfully lead and deliver complex projects. Learn how to tackle challenging project scenarios, optimize resource allocation, and ensure project success from start to finish. ",
+},
+// Term 3 Courses
+{
+  id: 8,
+  name: "Advanced Project management1",
+  code: "Pr333",
+  description:
+    "This comprehensive program is designed to empower experienced project managers with the advanced tools, techniques, and strategies needed to tackle complex projects and lead teams to success. Explore topics such as risk management, agile methodologies, stakeholder engagement, and advanced project analytics.",
+},
+{
+  id: 9,
+  name: "Advanced C++ Programming Fundamentals",
+  code: "C++333",
+  description:
+    "Explore topics such as template metaprogramming, advanced data structures, memory management, and performance optimization, and gain the expertise needed to write efficient, scalable, and error-free C++ code. ",
+},
+{
+  id: 10,
+  name: "Advanced Computer Maintenance",
+  code: "CompM333",
+  description:
+    " This comprehensive program is designed for tech-savvy individuals, IT professionals, and computer enthusiasts looking to deepen their understanding of hardware and software troubleshooting, system optimization, and preventive maintenance techniques. Dive into advanced diagnostics, hardware upgrades, data recovery, and explore the latest tools and techniques to keep your computers running smoothly.",
+},
+{
+  id: 11,
+  name: "Advanced Information Security1",
+  code: "IS333",
+  description:
+    "Explore advanced topics such as threat intelligence, penetration testing, cryptography, network security, and more. Learn to analyze and mitigate complex cyber threats, understand the latest hacking techniques, and strengthen your organization's defenses. ",
+},
+
+// Term 4 Courses
+{
+  id: 12,
+  name: "Advanced Networking",
+  code: "Net444",
+  description:
+    " Designed for professionals and enthusiasts eager to take their networking knowledge to the next level, this course delves into the cutting-edge technologies and strategies that power the digital world. From virtualization and software-defined networks to cybersecurity and cloud integration, you'll gain the expertise needed to design, optimize, and secure complex network infrastructures.",
+},
+{
+  id: 13,
+  name: "Advanced Web technology",
+  code: "Web444",
+  description:
+    "Explore cutting-edge web development tools, techniques, and trends to master the art of creating dynamic and interactive websites. From responsive design to the latest in web security, this course equips you with the knowledge and skills needed to stay ahead in the rapidly evolving world of web technology.",
+},
+{
+  id: 14,
+  name: "Advanced Project Management2",
+  code: "Pro444",
+  description:
+    "Building upon the foundational principles of project management, this course delves into advanced techniques and strategies to successfully plan, execute, and close complex projects. Elevate your career and project management capabilities with Advanced Project Management 2.",
+},
 ];
 
 export default coursesData;
